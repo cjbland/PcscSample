eec10bf0bdd3ef240293adb96d8dc6dcf8087f47
Christopher Bland <cjbland@gmail.com>
2023-10-18 18:41:57 -0400

    Tweaks to get apk to build

--- app/src/androidTest/java/fr/coppernic/sample/pcsc/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/fr/coppernic/sample/pcsc/ExampleInstrumentedTest.java
@@ -1,11 +1,12 @@
 package fr.coppernic.sample.pcsc;
 
 import android.content.Context;
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import static org.junit.Assert.assertEquals;
 
@@ -19,7 +20,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("fr.coppernic.sample.pcsc", appContext.getPackageName());
     }
